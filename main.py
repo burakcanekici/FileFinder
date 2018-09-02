249188d34108c8c3dc2157505f4570074eaa760a
burakcanekici <bcekici11@gmail.com>
2018-09-02 21:07:20 +0300

    Find Operation by Extension
    find by extension

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os,sys
 
-def traverseAllAndFindFile(root, desire):
+def traverseAllAndFindFile(root, desire, isExt):
     count = 1
     for directoryName, subDirList, subDirectoryList in os.walk(root):
         for file in subDirectoryList:
-            if(file == desire):
+            item = file
+            if(isExt == 1):
+                fname, fext = os.path.splitext(file)
+                item = fext
+            if(item == desire):
                 path = directoryName + '/' + file
                 print('[%d] %s' %(count, path))
                 count = count+1
-    print("----- Finished Successfuly! -----")
+    print('----- Finished Successfuly! -----')
 
 operation = sys.argv[1]
 if(operation == 'F'):
     root = sys.argv[2]
-    desireFileName = sys.argv[3]
-    traverseAllAndFindFile(root, desireFileName)
+    param = sys.argv[3]
+    ext = 0
+    if param[:1] == '.':
+        ext = 1
+    traverseAllAndFindFile(root, param, ext)
